8ef604e1e87832f55c39c84b45aa80970f608273
Dehao <clydeu2018@gmail.com>
2020-09-15 19:26:26 -0500

    5

--- codes/#5/code.py
+++ codes/#5/code.py
@@ -1,3 +1,18 @@
 def longestPalindrome(self, s: str) -> str:
 
-    pass
+    length=len(str)
+    if length<2:
+        return str
+    dp=[[None *length]*length]
+    for y in range(length):
+        for x in range(y):
+
+            dp[x][y]=str[x]==str[y] and (y-x<3 or dp[x+1][y-1])
+    
+        
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
